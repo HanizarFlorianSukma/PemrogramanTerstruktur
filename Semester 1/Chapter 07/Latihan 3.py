8bbdd0c6a29f18e911788eb26448a2f09dfdc9fa
Hanizarflorians <75709880+HanizarFlorianSukma@users.noreply.github.com>
2021-11-05 10:22:25 +0700

    Update Latihan 3.py

--- Semester 1/Chapter 07/Latihan 3.py
+++ Semester 1/Chapter 07/Latihan 3.py
@@ -1,3 +1,4 @@
+# ============================================   FUNCTION FOR AVERAGE   ===========================================
 def average(num):
     sum_num = 0
     for data in num:
@@ -5,11 +6,15 @@ def average(num):
     average = sum_num / len(num)
     return average
 
+# ===============================================   FUNCTION FOR MAIN  ============================================
 def main():
     try:
+        print("\n===============================")
+        print("   Program Calculate Average   ")
+        print("===============================")
         num = int(input('\nEnter integer number : '))
         list_num.append(num)
-        again = input("\nAgain [y/n] ? : ")
+        again = input("\nEnter numbers again [y/n] ? : ")
         if again == 'y':
             main()
         elif again.lower() == 'n':
@@ -19,8 +24,18 @@ def main():
             print("\nInput unknown\nPlease enter the correct choice!\n")
             main()
     except ValueError:
-        print('\nNumbers not an integer')
+        print('\nInput is not an integer')
         main()
 
+# ==============================================   FUNCTION FOR START   ===========================================
+def start():
+    ask = input('Are you ready to Start this program [y/n] ? ')
+    if ask.lower() == 'y':
+        main()
+    else:
+        exit()
+
+# ========================================   LIST TO ACCOMMOD ALL NUMBERS  ========================================
 list_num = []
-main()
+
+start()
