f33b4433c10c70ba14bb0f6e61f74783fbad8ba5
Hanizarflorians <75709880+HanizarFlorianSukma@users.noreply.github.com>
2021-11-22 00:19:14 +0700

    Update Project 2.py

--- Semester 1/Chapter 08/Project 2.py
+++ Semester 1/Chapter 08/Project 2.py
@@ -6,8 +6,6 @@ try:
     for x in range(n):
         num = int(input(f'Bilangan bulat ke-{x+1} : '))
         bil.append(num)
-    print(f'\nRata rata dari list {bil}        : {average(bil)}')
-    print(f'Nilai tertinggi dari list {bil}  : {sorted(bil)[-1]}')
-    print(f'Nilai terrendah dari list {bil}  : {sorted(bil)[0]}')
+    print(f'\nRata rata dari list {bil}        : {average(bil)}\nNilai tertinggi dari list {bil}  : {sorted(bil)[-1]}\nNilai terrendah dari list {bil}  : {sorted(bil)[0]}')
 except ValueError:
     print('Input yang dimasukkan bukan berupa angka')
