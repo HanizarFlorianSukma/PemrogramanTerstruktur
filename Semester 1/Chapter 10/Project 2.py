9ecf220b1f44ba61dc33629abdd9f961b4cfce4d
Hanizarflorians <75709880+HanizarFlorianSukma@users.noreply.github.com>
2021-11-30 16:50:15 +0700

    Create Project 2.py

---
+++ Semester 1/Chapter 10/Project 2.py
@@ -0,0 +1,10 @@
+file = open("d:/file2.txt", "a+")
+while True:
+    nim = input('\nMasukkan NIM      : ')
+    nama = input('Masukkan Nama Mhs : ')
+    alamat = input('Alamat            : ')
+    file.write((f'{nim}|{nama}|{alamat}\n')); file.seek(0,0)
+    ulangi = input('\nUlangi input lagi [y/n] :')
+    if ulangi.lower() not in 'y': break
+print(f'\n{file.read()}')
+file.close()
