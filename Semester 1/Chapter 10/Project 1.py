8a3fc16c8061e459a7ea33cf2ed7806436c07cf8
Hanizarflorians <75709880+HanizarFlorianSukma@users.noreply.github.com>
2021-12-01 13:38:34 +0700

    Update Project 1.py

--- Semester 1/Chapter 10/Project 1.py
+++ Semester 1/Chapter 10/Project 1.py
@@ -1,13 +1,20 @@
+# Membuka file dengan mode untuk di baca
 file = open("d:/file1.txt", "r")
-genap = 0
+
+# Variable
 ganjil = 0
+genap = 0
+
+# Looping for untuk mengubah type data teks di dalam file, dan memilih ganjil genapnya
 for i in file:
-    newContent = int(i)
-    if newContent%2 == 0:
+    teksIsInt = int(i)
+    if teksIsInt%2 == 0:
         genap += 1
     else:
         ganjil += 1
 
 print(f'Banyaknya bilangan genap  : {genap}')
 print(f'Banyaknya bilangan ganjil : {ganjil}')
+
+# Menuutp kembali file
 file.close()
