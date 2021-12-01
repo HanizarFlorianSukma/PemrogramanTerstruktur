b9fec85d34fbb89d5a2815314d54da5356b08ad8
Hanizarflorians <75709880+HanizarFlorianSukma@users.noreply.github.com>
2021-12-01 13:49:49 +0700

    Update Project 7.py

--- Semester 1/Chapter 10/Project 7.py
+++ Semester 1/Chapter 10/Project 7.py
@@ -1,17 +1,24 @@
-file = open("d:jawabanNo6.txt", "r")
-teksSandi = (file.read())
-n = 2
-print(f'Teks Sandi : {teksSandi}\nNilai N    : {n}')
-teksAsli = ''
-for i in teksSandi:
-    value = ord(i)
-    newValue = value-n
-    if newValue == 30:
-        newValue = 32
-    elif newValue < 65:
-        newValue = 90 - (64-newValue)
-    teksAsli += (chr(newValue))
-file = open("d:jawabanNo7.txt", "a+")
-file.write(f'{teksAsli}')
-print(f'Teks Asli  : {teksAsli}')
-file.close()
+try:
+    filename = input("Path File  : ")
+    teksSandi = open(filename, "r").read()
+    n = int(input('Nilai N    : '))
+    
+    print(f'\nTeks Sandi : {teksSandi}')
+    teksAsli = ''
+    for i in teksSandi:
+        value = ord(i)
+        newValue = value-n
+        if newValue == 30:
+            newValue = 32
+        elif newValue < 65:
+            newValue = 90 - (64-newValue)
+        teksAsli += (chr(newValue))
+    file = open("d:dekripsi.txt", "a+")
+    file.write(f'{teksAsli}')
+    print(f'Teks Asli  : {teksAsli}')
+    file.close()
+    
+except FileNotFoundError:
+    print('File tidak ditemukan')
+except PermissionError:
+    print('File tidak ditemukan') 
