c2aba7c7cd638da32bc4f20ea00a905e129f7480
Hanizarflorians <75709880+HanizarFlorianSukma@users.noreply.github.com>
2021-12-01 13:39:21 +0700

    Update Project 2.py

--- Semester 1/Chapter 10/Project 2.py
+++ Semester 1/Chapter 10/Project 2.py
@@ -1,10 +1,18 @@
+# Membuka file dengan mode untuk di tulis dan dibaca ditambah seek
 file = open("d:/file2.txt", "a+")
+
+# Looping while digunakan untuk mengulangi program, jika tidak diminta untuk break
 while True:
     nim = input('\nMasukkan NIM      : ')
     nama = input('Masukkan Nama Mhs : ')
     alamat = input('Alamat            : ')
-    file.write((f'{nim}|{nama}|{alamat}\n')); file.seek(0,0)
+    file.write((f'{nim}|{nama}|{alamat}\n'));
+    file.seek(0,0)
     ulangi = input('\nUlangi input lagi [y/n] :')
     if ulangi.lower() not in 'y': break
+
+# Membaca file
 print(f'\n{file.read()}')
+
+# Menutup file
 file.close()
